307910869f7d1da0874be52cdf639f96590b419d
Odesii <alic.madd@gmail.com>
2024-04-30 21:25:56 -0400

    continuing to add the base files and models

---
+++ models/Tag.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
